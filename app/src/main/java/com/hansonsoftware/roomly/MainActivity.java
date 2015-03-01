f873a4bb1a6fcc1afccd164e36efd681e11e6133
Daniel Hanson <dhansonmu@gmail.com>
2015-03-01 01:47:27 -0600

    First commit

---
+++ app/src/main/java/com/hansonsoftware/roomly/MainActivity.java
@@ -0,0 +1,46 @@
+package com.hansonsoftware.roomly;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Sets the content view
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // References the text view from the XML and sets the font
+        TextView myTextView = (TextView) findViewById(R.id.textView);
+        Typeface typeFace = Typeface.createFromAsset(getAssets(),"fonts/ostrich-sans/ostrich-rounded.ttf");
+        myTextView.setTypeface(typeFace);
+
+        // References the button from the XML, sets the font, and makes a fade in animation
+        Button button = (Button) findViewById(R.id.button);
+        Typeface typeFaceBold = Typeface.createFromAsset(getAssets(),"fonts/ostrich-sans/ostrich-black.ttf");
+        button.setTypeface(typeFaceBold);
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new AccelerateInterpolator()); //add this
+        fadeIn.setDuration(2000);
+        button.startAnimation(fadeIn);
+
+        // Adds click listener for the button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
